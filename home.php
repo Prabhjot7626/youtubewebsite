d3638f2826f4111100fe0a9ab8a956026dcc8d05
Prabhjot <prabh.kaurmankoo@gmail.com>
2023-03-22 11:33:42 +0530

    new

---
+++ home.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swipre css link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js" integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!--custom css file link-->
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <!--<style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap');
+
+        :root {
+            --main-color: #8e44ad;
+            --black: #222;
+            --white: #fff;
+            --light-black: #777;
+            --light-white: #fff9;
+            --dark-bg: rgba(0, 0, 0, .7);
+            --light-bg: #eee;
+            --border: .1rem solid var(--black);
+            --box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .1);
+            --text-shadow: 0 1.5rem 3rem rgba(0, 0, 0, .3);
+        }
+
+        * {
+            font-family: 'Poppins', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            outline: none;
+            border: none;
+            text-decoration: none;
+            text-transform: capitalize;
+        }
+
+        html {
+            font-size: 62.5%;
+            overflow-x: hidden;
+        }
+
+        html::-webkit-scrollbar {
+            width: 1rem;
+        }
+
+        html::-webkit-scrollbar-track {
+            background-color: var(--white);
+        }
+
+        html::-webkit-scrollbar-thumb {
+            background-color: var(--main-color);
+        }
+
+        section {
+            padding: 5rem 10%;
+        }
+
+        .header {
+            position: sticky;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 1000;
+            background-color: var(--white);
+            display: flex;
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+            box-shadow: var(--box-shadow);
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .header .logo {
+            font-size: 2.5rem;
+            color: var(--black);
+        }
+
+        .header .navbar a {
+            font-size: 2rem;
+            margin-left: 2rem;
+            color: var(--black);
+        }
+
+        .header .navbar a:hover {
+            color: var(--main-color);
+        }
+    </style> -->
+    <link rel="stylesheet" href="style.css">        
+</head>
+
+<body>
+
+    <!--header section starts-->
+
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+
+    </section>
+
+
+    <!--header section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i>home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i>about</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i>package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>terms of use</a>
+        </div>
+
+        <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i>+123-456-7890</a>
+        <a href="#"> <i class="fas fa-phone"></i>+111-222-3333</a>
+        <a href="#"> <i class="fas fa-envelope"></i>shaikhanas@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"> <i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"> <i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"> <i class="fab fa-linkedin"></i>linkedin</a>
+    </div>
+    </div>
+    </section>
+
+
+<!--footer section ends-->
+
+
+
+
+    <!-- swiper js link-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!--custom js file link-->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
